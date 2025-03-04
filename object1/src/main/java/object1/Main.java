b55406d4650fef7d1ed34f3b222418d874c447f7
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-03 22:33:00 -0500

    finished - array & class playhouse

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -1,10 +1,24 @@
 package object1;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String... args) {
-        Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+
+
+        Person person3 = new Person("judy",30,172);
+        System.out.println(person3.height);
+
+        Person[] people = new Person[10];
+        Person person1 = new Person("Alice", 25,134);
+        Person person2 = new Person("Bob", 30,778);
+
+        people[0] = person1;
+        people[1] = person2;
+
+        System.out.println(Arrays.toString(people));
+
+        }
 }
